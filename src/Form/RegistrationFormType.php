ba6288dde87075fb5fa1413cf8117ea91bb6a1a5
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2026-03-18 10:24:20 +0100

    add double password

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,26 +17,25 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class, [
-                'label'       => 'Nom',
-                'attr'        => ['placeholder' => 'Votre nom'],
-                'constraints' => [new NotBlank(message: 'Veuillez entrer votre nom')],
-            ])
-            ->add('prenom', TextType::class, [
-                'label'       => 'Prénom',
-                'attr'        => ['placeholder' => 'Votre prénom'],
-                'constraints' => [new NotBlank(message: 'Veuillez entrer votre prénom')],
-            ])
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
-                'mapped'      => false,
+                'mapped' => false,
                 'constraints' => [
                     new IsTrue(message: 'You should agree to our terms.'),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                'mapped'      => false,
-                'attr'        => ['autocomplete' => 'new-password'],
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options'  => [
+                    'label' => 'Mot de passe',
+                    'attr' => ['id' => 'password-field']
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                    'attr' => ['id' => 'confirm-password-field']
+                ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas',
+                'mapped' => false,
                 'constraints' => [
                     new NotBlank(message: 'Please enter a password'),
                     new Length(
